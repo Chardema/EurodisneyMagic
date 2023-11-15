6aca1b4875c2ca3675fb842a8c94805233038b4c
HARDEMAN CHRISTOPHER <41175936+Chardema@users.noreply.github.com>
2023-11-15 11:29:36 +0100

    Mise à jour pour mobile !! Une slide et ajout de TOUTES LES IMAGES

--- src/Home/Homepage.js
+++ src/Home/Homepage.js
@@ -3,12 +3,41 @@ import Slider from 'react-slick';
 import axios from "axios";
 import DRSDiscoveryland from './../img/Disneyland Railroad Discoveryland Station.jpg';
 import DRSFrontierland from './../img/Disneyland Railroad Frontierland Depot.jpg';
+import DRSMainStreet from './../img/MainstreetStation.jpg'
 import Orbitron from './../img/Orbitron.jpg';
 import MeetMickey from './../img/MeetMickey.jpg';
 import FrontierLandPg from './../img/FrontierlandPlayground.jpg';
 import GalionPirate from './../img/PirateGalleon.jpg'
 import DRSFantasyland from './../img/FantasylandRailRoad.jpg';
-import IndianaJones from './../img/IndianaJonesetletempleduperil.jpg'
+import DR from './../img/DisneylandRailroad.jpg'
+import Pirates from './../img/Pirateofthecarribean.jpg'
+import PP from './../img/Pavillon Princesse.jpg'
+import IndianaJones from './../img/IndianaJonesetletempleduperil.jpg';
+import CabaneRobinson from './../img/CabaneRobinson.JPG';
+import PhantomManor from './../img/Phantommanor.jpg'
+import ISW from './../img/its a small world.jpg'
+import StarTours from './../img/Startour.jpg'
+import HSM from './../img/Hyperspacemountain.jpg'
+import MN from './../img/Nautilus.jpg'
+import Caroussel from './../img/Carroussellancelot.jpg'
+import BTM from './../img/BTM.jpg';
+import PPF from './../img/Peterpanflight.jpg'
+import DFE from './../img/Dumboflying.jpg'
+import PEA from './../img/Passagealadin.jpg'
+import autopia from './../img/Autopia.jpg'
+import BlancheNeige from './../img/BlancheNeige.jpg'
+import AdventureIsle from './../img/AdventureIsle.jpg'
+import StartPort from './../img/Startport.jpg'
+import Alice from './../img/Alicelabyrinthe.jpg'
+import Buzz from './../img/buzz lightyear.jpg'
+import MHTC from './../img/TeaCup.jpg'
+import RRSG from './../img/ShootinGallery.jpg'
+import TD from './../img/Tanière dragon.jpg'
+import MickeyPhil from './../img/orchestrephilarmagique.jpg'
+import ThunderMesa from './../img/ThunderMesa.jpg'
+import Pinocchio from './../img/Pinocchio.jpg'
+import Casey from './../img/CaseyJr.jpg'
+import MSV from './../img/Mainstreetvehicles.jpg'
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import ThemeParkInfo from './../ThemeParkInfo';
@@ -18,13 +47,42 @@ import useInterval from './../useInterval'; // Assurez-vous d'importer useInterv
 
 const attractionImages = {
     'Disneyland Railroad Discoveryland Station': DRSDiscoveryland,
+    'Disneyland Railroad Fantasyland Station': DRSFantasyland,
+    'Disneyland Railroad Main Street Station': DRSMainStreet,
+    'Disneyland Railroad': DR,
     'Orbitron®': Orbitron,
     'Meet Mickey Mouse': MeetMickey,
     'Frontierland Playground': FrontierLandPg,
     'Disneyland Railroad Frontierland Depot': DRSFrontierland,
     'Pirate Galleon': GalionPirate,
-    'Disneyland Railroad Fantasyland Station': DRSFantasyland,
     'Indiana Jones™ and the Temple of Peril':IndianaJones,
+    'La Cabane des Robinson':CabaneRobinson,
+    'Big Thunder Mountain':BTM,
+    "Mad Hatter's Tea Cups":MHTC,
+    'Les Voyages de Pinocchio':Pinocchio,
+    'Casey Jr. – le Petit Train du Cirque':Casey,
+    'Phantom Manor': PhantomManor,
+    'Star Wars Hyperspace Mountain':HSM,
+    'Star Tours: The Adventures Continue*':StarTours,
+    'Thunder Mesa Riverboat Landing':ThunderMesa,
+    "Alice's Curious Labyrinth":Alice,
+    "Buzz Lightyear Laser Blast":Buzz,
+    'Main Street Vehicles':MSV,
+    "Peter Pan's Flight": PPF,
+    'Princess Pavilion':PP,
+    'Dumbo the Flying Elephant':DFE,
+    "Le Passage Enchanté d'Aladdin":PEA,
+    'Autopia®': autopia,
+    'Le Carrousel de Lancelot ': Caroussel,
+    'Les Mystères du Nautilus' :MN,
+    'La Tanière du Dragon': TD,
+    "Rustler Roundup Shootin' Gallery":RRSG,
+    'Adventure Isle': AdventureIsle,
+    'Welcome to Starport: A Star Wars Encounter': StartPort,
+    "Blanche-Neige et les Sept Nains®": BlancheNeige,
+    "Mickey’s PhilharMagic": MickeyPhil,
+    'Pirates of the Caribbean': Pirates,
+    '"it\'s a small world"': ISW
 };
 
 function Homepage() {
@@ -39,6 +97,29 @@ function Homepage() {
         speed: 500,
         slidesToShow: 3,
         slidesToScroll: 3,
+        responsive: [
+            {
+                breakpoint: 1024, // Large desktop
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                },
+            },
+            {
+                breakpoint: 768, // Tablet
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                },
+            },
+            {
+                breakpoint: 480, // Mobile
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                },
+            },
+        ],
     };
 
     // Utilisez le hook useInterval pour effectuer un appel API à intervalles réguliers
@@ -79,8 +160,9 @@ function Homepage() {
     }, [rawRideData]);
 
     return (
-        <div className="container">
+        <div className="Home">
             <Navbar />
+        <div className="container">
             <p className="lastUpdate">{lastUpdate ? `Dernière mise à jour : ${formatDate(lastUpdate)}` : 'Aucune mise à jour récente'}</p>
             <ThemeParkInfo setRawRideData={setRawRideData} />
             <div className="sliderContainer">
@@ -114,6 +196,7 @@ function Homepage() {
                     </Slider>
                 </div>
             </div>
+        </div>
         </div>
     );
 }
