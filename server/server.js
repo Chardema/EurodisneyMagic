f984d62a09d88c01ba1d948dd6b6f50d562f9608
HARDEMAN CHRISTOPHER <41175936+Chardema@users.noreply.github.com>
2023-11-19 20:27:13 +0100

    Passage MySql

--- server/server.js
+++ server/server.js
@@ -1,41 +1,35 @@
 const express = require('express');
-const mongoose = require('mongoose');
+const mysql = require('mysql2');
 const axios = require('axios');
-const Ride = require('./models/Ride'); // Importez votre modèle de données Ride
 const cors = require('cors');
-const app = express();
 require('dotenv').config();
-const mongoURL = process.env.MONGO_URL
 
-// Utilisation du port fourni par l'environnement pour Azure, sinon 5000
-const port = process.env.PORT || 5000;
+const app = express();
 
 // Middleware pour gérer les requêtes JSON
 app.use(express.json());
 app.use(cors()); // Activation de CORS
 
-// Connexion à MongoDB Atlas
-mongoose.connect(mongoURL)
-    .then(() => console.log('Connecté à MongoDB Atlas'))
-    .catch(err => console.error('Erreur de connexion à MongoDB Atlas:', err));
+// Configuration de la connexion MySQL
+const pool = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASS,
+});
 
+const port = process.env.PORT || 5000;
 // Fonction pour supprimer les attractions obsolètes
-const removeObsoleteAttractions = async () => {
+const getAttractions = async () => {
     try {
-        const obsoleteAttractions = await Ride.find({
-            $or: [{ entityType: { $ne: 'ATTRACTION' } }]
-        });
-
-        for (const attraction of obsoleteAttractions) {
-            await Ride.deleteOne({ _id: attraction._id });
-        }
-
-        console.log(`Attractions obsolètes supprimées : ${obsoleteAttractions.length}`);
+        const [rows] = await pool.promise().query('SELECT * FROM attractions');
+        return rows;
     } catch (error) {
-        console.error('Erreur lors de la suppression des attractions obsolètes:', error);
+        console.error('Erreur lors de la récupération des attractions:', error);
+        throw error;
     }
 };
-removeObsoleteAttractions();
+
 
 // Fonction pour récupérer les données depuis l'API externe et les stocker dans MongoDB
 const fetchAndStoreParkData = async (parkUrl) => {
@@ -45,34 +39,52 @@ const fetchAndStoreParkData = async (parkUrl) => {
 
         for (const rideData of rideTimes) {
             if (rideData.entityType === 'ATTRACTION') {
-                await Ride.findOneAndUpdate(
-                    { id: rideData.id },
-                    {
-                        id: rideData.id,
-                        name: rideData.name,
-                        status: rideData.status,
-                        entityType: rideData.entityType,
-                        queue: {
-                            STANDBY: {
-                                waitTime: rideData.queue?.STANDBY?.waitTime || null
-                            }
-                        },
-                        parkId: rideData.parkId,
-                        externalId: rideData.externalId,
-                        lastUpdated: new Date(rideData.lastUpdated)
-                    },
-                    { upsert: true }
-                );
+                // Vérifiez si l'attraction existe déjà
+                const [rows] = await pool.promise().query('SELECT id FROM attractions WHERE id = ?', [rideData.id]);
+
+                if (rows.length > 0) {
+                    // Mise à jour de l'attraction existante
+                    await pool.promise().query(
+                        'UPDATE attractions SET name = ?, status = ?, entityType = ?, standbyWaitTime = ?, parkId = ?, externalId = ?, lastUpdated = ? WHERE id = ?',
+                        [
+                            rideData.name,
+                            rideData.status,
+                            rideData.entityType,
+                            rideData.queue?.STANDBY?.waitTime || null,
+                            rideData.parkId,
+                            rideData.externalId,
+                            new Date(rideData.lastUpdated),
+                            rideData.id
+                        ]
+                    );
+                } else {
+                    // Insertion d'une nouvelle attraction
+                    await pool.promise().query(
+                        'INSERT INTO attractions (id, name, status, entityType, standbyWaitTime, parkId, externalId, lastUpdated) VALUES (?, ?, ?, ?, ?, ?, ?, ?)',
+                        [
+                            rideData.id,
+                            rideData.name,
+                            rideData.status,
+                            rideData.entityType,
+                            rideData.queue?.STANDBY?.waitTime || null,
+                            rideData.parkId,
+                            rideData.externalId,
+                            new Date(rideData.lastUpdated)
+                        ]
+                    );
+                }
             } else {
-                await Ride.deleteOne({ id: rideData.id });
+                // Supprimer l'attraction si elle n'est pas du type "ATTRACTION"
+                await pool.promise().query('DELETE FROM attractions WHERE id = ?', [rideData.id]);
             }
         }
 
-        console.log('Données mises à jour dans MongoDB');
+        console.log('Données mises à jour dans MySQL');
     } catch (error) {
         console.error('Erreur lors de la récupération et du stockage des données:', error);
     }
 };
+
 
 const fetchAndStoreData = async () => {
     await fetchAndStoreParkData('https://api.themeparks.wiki/v1/entity/dae968d5-630d-4719-8b06-3d107e944401/live');
@@ -82,10 +94,10 @@ const fetchAndStoreData = async () => {
 // Exécutez la fonction de récupération et de stockage des données à intervalles réguliers
 setInterval(fetchAndStoreData, 60000);
 
-// Route pour récupérer toutes les attractions depuis MongoDB
+// Route pour récupérer toutes les attractions depuis MySQL
 app.get('/api/attractions', async (req, res) => {
     try {
-        const attractions = await Ride.find();
+        const attractions = await getAttractions();
         res.json(attractions);
     } catch (error) {
         console.error('Erreur lors de la récupération des attractions:', error);
