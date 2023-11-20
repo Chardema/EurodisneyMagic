9eba9deadd7b0eb0ccfcb24f7c239d71a184fc26
HARDEMAN CHRISTOPHER <41175936+Chardema@users.noreply.github.com>
2023-11-20 21:53:26 +0100

    Ajout horraire studio // next : harmoniser

--- src/Home/Homepage.js
+++ src/Home/Homepage.js
@@ -3,53 +3,42 @@ import { Link } from 'react-router-dom';
 import Navbar from "../Navbar/Navbar";
 import styles from './Homepage.module.scss';
 import useParkHours from "../FetchParkHours";
-import Castle from './../img/DisneylandCastle.png';
+import Castle from './../img/Disneylandlogo.png';
+import Studios from './../img/Studioslogo.png';
 import { formatTime } from '../utils';
 
 const Homepage = () => {
     const parkHours = useParkHours();
     const now = new Date();
 
-    const renderParkHours = () => {
-        if (!parkHours || !parkHours.schedule) {
-            return <p>Horaires non disponibles.</p>;
+    const renderScheduleInfo = (schedules, parkName) => {
+        if (!schedules) {
+            return <p>Horaires de {parkName} non disponibles.</p>;
         }
 
         const todayStr = now.toISOString().split('T')[0];
-        const todaySchedules = parkHours.schedule.filter(schedule => schedule.date === todayStr);
+        const todaySchedules = schedules.filter(schedule => schedule.date === todayStr);
+        const operatingSchedule = todaySchedules.find(s => s.type === "OPERATING");
+        const extraHoursSchedule = todaySchedules.find(s => s.type === "EXTRA_HOURS");
 
-        const isParkOpen = (schedules) => {
-            const operatingSchedule = schedules.find(s => s.type === "OPERATING");
-            if (!operatingSchedule) return false;
+        if (!operatingSchedule) {
+            return <p className={styles.closedMessage}>{parkName} est actuellement fermé.</p>;
+        }
 
-            const openingTime = new Date(todayStr + 'T' + operatingSchedule.openingTime);
-            const closingTime = new Date(todayStr + 'T' + operatingSchedule.closingTime);
-            return now >= openingTime && now <= closingTime;
-        };
+        const openingTime = new Date(todayStr + 'T' + operatingSchedule.openingTime);
+        const closingTime = new Date(todayStr + 'T' + operatingSchedule.closingTime);
+        const isParkOpen = now >= openingTime && now <= closingTime;
 
-        const renderScheduleInfo = (schedules) => {
-            if (!isParkOpen(schedules)) {
-                return <p className={styles.closedMessage}>Le Parc Disneyland est actuellement fermé.</p>;
-            }
-
-            const operatingSchedule = schedules.find(s => s.type === "OPERATING");
-            const extraHoursSchedule = schedules.find(s => s.type === "EXTRA_HOURS");
-
-            return (
-                <>
-                    {operatingSchedule && (
-                        <p className={styles.schedule}>Aujourd'hui, Le Parc Disneyland est ouvert entre {formatTime(operatingSchedule.openingTime)} et {formatTime(operatingSchedule.closingTime)}.</p>
-                    )}
-                    {extraHoursSchedule && (
-                        <p className={styles.schedule}>Avec des Magic Hours entre {formatTime(extraHoursSchedule.openingTime)} et {formatTime(extraHoursSchedule.closingTime)}.</p>
-                    )}
-                </>
-            );
-        };
+        if (!isParkOpen) {
+            return <p className={styles.closedMessage}>{parkName} est actuellement fermé.</p>;
+        }
 
         return (
             <>
-                {renderScheduleInfo(todaySchedules)}
+                <p className={styles.schedule}>Aujourd'hui, {parkName} est ouvert entre {formatTime(operatingSchedule.openingTime)} et {formatTime(operatingSchedule.closingTime)}.</p>
+                {extraHoursSchedule && (
+                    <p className={styles.schedule}>Avec des Magic Hours entre {formatTime(extraHoursSchedule.openingTime)} et {formatTime(extraHoursSchedule.closingTime)}.</p>
+                )}
             </>
         );
     };
@@ -58,11 +47,20 @@ const Homepage = () => {
         <div className={styles.body}>
             <Navbar />
             <div className={styles.container}>
-                <div className={styles.hours}>
-                    <img className={styles.logo} src={Castle} alt="Castle Hours" />
-                    {renderParkHours()}
+                <div className={styles.allparks}>
+                    <div className={styles.disneyland}>
+                        <div className={styles.hours}>
+                            <img className={styles.logo} src={Castle} alt="Disneyland Park Hours" />
+                            {renderScheduleInfo(parkHours?.disneyland?.schedule, "Le Parc Disneyland")}
+                        </div>
+                    </div>
+                    <div className={styles.studios}>
+                        <div className={styles.hours}>
+                            <img className={styles.logo} src={Studios} alt="Studio Park Hours" />
+                            {renderScheduleInfo(parkHours?.studio?.schedule, "Le Parc Walt Disney Studios")}
+                        </div>
+                    </div>
                 </div>
-                {/* Bouton pour naviguer vers les attractions */}
                 <div className={styles.centerButton}>
                     <Link to="/attractions" className={styles.attractionsButton}>Temps d'attente en direct</Link>
                 </div>
