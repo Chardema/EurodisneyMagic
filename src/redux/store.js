6912ca1ec7684669672092da8bc6fce33cf7600e
HARDEMAN CHRISTOPHER <41175936+Chardema@users.noreply.github.com>
2023-12-09 22:15:48 +0100

    Travail sur le localstorage (ok normalement)

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,14 @@
 // store.js
-import { createStore } from 'redux';
-import Attractionreducer from "./reducer/Attractionreducer";
+// store.js
+import { createStore, combineReducers } from 'redux';
+import attractionsReducer from "./reducer/Attractionreducer";  // Assurez-vous que ce nom correspond à l'exportation par défaut de votre fichier reducer
+import favoritesReducer from "./reducer/favoriteReducer";
 
-const store = createStore(Attractionreducer);
+const rootReducer = combineReducers({
+    attractions: attractionsReducer,
+    favorites: favoritesReducer,
+});
+
+const store = createStore(rootReducer);  // Utilisez rootReducer ici
 
 export default store;
