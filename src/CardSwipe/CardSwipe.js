5b6f551709d9f3551a25bc7a765056b138ac6d4c
Hardeman C <christo59@icloud.com>
2024-03-15 16:40:18 +0100

    Swipe OK

--- src/CardSwipe/CardSwipe.js
+++ src/CardSwipe/CardSwipe.js
@@ -1,22 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSwipeable } from 'react-swipeable';
-import styles from './CardSwipe.module.scss'; // Assurez-vous de créer ou d'ajuster ce fichier CSS
+import styles from './CardSwipe.module.scss';
 
 const CardSwipe = ({ attraction, onSwipeLeft, onSwipeRight }) => {
+  const [swipeDirection, setSwipeDirection] = useState('');
+
   const handlers = useSwipeable({
-    onSwipedLeft: () => onSwipeLeft(attraction),
-    onSwipedRight: () => onSwipeRight(attraction),
-    preventDefaultTouchmoveEvent: true,
+    onSwiping: (eventData) => {
+      setSwipeDirection(eventData.dir);
+    },
+    onSwipedLeft: () => {
+      setSwipeDirection('');
+      onSwipeLeft(attraction);
+    },
+    onSwipedRight: () => {
+      setSwipeDirection('');
+      onSwipeRight(attraction);
+    },
     trackMouse: true
   });
 
+  let cardStyle = {};
+  if (swipeDirection === 'Left') {
+    cardStyle = { transform: 'rotate(-10deg)' };
+  } else if (swipeDirection === 'Right') {
+    cardStyle = { transform: 'rotate(10deg)' };
+  }
+
   return (
-    <div {...handlers} className={styles.card}>
+    <div {...handlers} className={styles.card} style={cardStyle}>
       <img src={attraction.image} alt={attraction.name} className={styles.image} />
-      <h3 className={styles.title}>{attraction.name}</h3>
+      <div className={styles.details}>
+        <h3>{attraction.name}</h3>
+      </div>
     </div>
   );
 };
 
 export default CardSwipe;
-
