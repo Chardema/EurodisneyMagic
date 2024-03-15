edd88245ed8ca7a62c17dcce72811f968abccca1
Hardeman C <christo59@icloud.com>
2024-03-15 12:22:49 +0100

    Changement sur homepage

--- src/Home/appHome.js
+++ src/Home/appHome.js
@@ -1,9 +1,9 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 import { Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { RiDeleteBin7Fill } from "react-icons/ri";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHeart as solidHeart, faHeart as regularHeart } from '@fortawesome/free-solid-svg-icons';
+import { faHeart as solidHeart } from '@fortawesome/free-solid-svg-icons';
 import Navbar from "../Navbar/Navbar";
 import BottomNav from "../mobileNavbar/mobileNavbar";
 import PopupSurvey from '../popupSurvey/popupSurvey';
@@ -12,147 +12,161 @@ import { useSwipeable } from 'react-swipeable';
 import backgroundImage from './../img/simphonyofcolor.jpg';
 import styles from './appHome.module.scss';
 import { attractionNames, attractionImages } from "../Attractions/AttractionsPage";
-
-// Assuming useWindowWidth is a custom hook you've created:
 import { useWindowWidth } from '../utils';
+
+
+
+
+const getWaitTimeColor = (attraction) => {
+    if (attraction.status === 'CLOSED' || attraction.status === 'DOWN') {
+      return 'gray';
+    } else if (attraction.waitTime <= 15) {
+      return 'green';
+    } else if (attraction.waitTime <= 30) {
+      return 'yellow';
+    } else {
+      return 'red';
+    }
+  };
 
 // Define the FavoriteCard component
 const FavoriteCard = ({ favorite, onRemove, getWaitTimeColor }) => {
-    const [swipeProgress, setSwipeProgress] = useState(0);
-    const [swipeAction, setSwipeAction] = useState(false);
+  const [swipeProgress, setSwipeProgress] = useState(0);
+  const [swipeAction, setSwipeAction] = useState(false);
+  const [showHint, setShowHint] = useState(true);
+  
 
-    const handlers = useSwipeable({
-        onSwiping: (eventData) => {
-            const progress = Math.abs(eventData.deltaX);
-            setSwipeProgress(progress);
-            setSwipeAction(eventData.dir === 'Left');
-        },
-        onSwiped: (eventData) => {
-            if (swipeAction && swipeProgress > window.innerWidth * 0.25) {
-                onRemove(favorite);
-            }
-            setSwipeProgress(0);
-            setSwipeAction(false);
-        },
-        trackMouse: true,
-    });
+  useEffect(() => {
+    // Disable the hint after the first display
+    const timer = setTimeout(() => {
+      setShowHint(false);
+    }, 1000); // Duration of the animation + delay
 
-    const indicatorStyle = {
-        width: swipeAction ? `${Math.min(swipeProgress / window.innerWidth * 100 * 2, 100)}%` : '0%',
-        opacity: swipeAction ? 1 : 0,
-    };
+    return () => clearTimeout(timer);
+  }, []);
 
-    return (
-        <div {...handlers} className={styles.attractionscard}>
-            <div className={styles.deleteIndicator} style={indicatorStyle}>
-                <div className={styles.deleteIndicatorContent}>
-                    Supprimer
-                </div>
-            </div>
-            <img src={attractionImages[favorite.name]} alt={favorite.name} className={styles.favoriteImage} />
-            <h3>{favorite.name}</h3>
-            <div className={styles.infoanddelete}>
-                <div className={`${styles.waitTimeCircle} ${getWaitTimeColor(favorite)}`}>
-                    {favorite.status === 'CLOSED' ? 'Fermée' :
-                    favorite.status === 'DOWN' ? 'Indispo' :
-                    favorite.waitTime === null ? 'Direct' : `${favorite.waitTime} min`}
-                </div>
-            </div>
+  const handlers = useSwipeable({
+    onSwiping: (eventData) => {
+      const progress = Math.abs(eventData.deltaX);
+      setSwipeProgress(progress);
+      setSwipeAction(eventData.dir === 'Left');
+    },
+    onSwiped: (eventData) => {
+      if (swipeAction && swipeProgress > window.innerWidth * 0.25) {
+        onRemove(favorite);
+      }
+      setSwipeProgress(0);
+      setSwipeAction(false);
+    },
+    trackMouse: true,
+  });
+
+  const indicatorStyle = {
+    width: swipeAction ? `${Math.min(swipeProgress / window.innerWidth * 100 * 2, 100)}%` : '0%',
+    opacity: swipeAction ? 1 : 0,
+  };
+
+  return (
+    <div {...handlers} className={`${styles.attractionscard} ${showHint ? 'swipeHintAnimation' : ''}`}>
+      <div className={styles.deleteIndicator} style={indicatorStyle}>
+        <div className={styles.deleteIndicatorContent}>
+          Supprimer
         </div>
-    );
+      </div>
+      <img src={attractionImages[favorite.name]} alt={favorite.name} className={styles.favoriteImage} />
+      <h3 className={styles.attractionTitle}>{favorite.name}</h3>
+      <div className={styles.waitTimeContainer}>
+      <div className={`${styles.waitTimeCircle} ${styles[getWaitTimeColor(favorite)]}`}>
+          {favorite.status === 'CLOSED' ? 'Fermée' :
+          favorite.status === 'DOWN' ? 'Indispo' :
+          favorite.waitTime === null ? 'Direct' : `${favorite.waitTime} min`}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 const HomePage = () => {
-    const reduxFavorites = useSelector(state => state.favorites.favorites);
-    const attractions = useSelector(state => state.attractions.attractions);
-    const dispatch = useDispatch();
-    const width = useWindowWidth(); // This needs to be a hook that returns the current window width
-    const [recommendedAttractions, setRecommendedAttractions] = useState([]);
-    const [showPopup, setShowPopup] = useState(false);
+  const reduxFavorites = useSelector(state => state.favorites.favorites);
+  const attractions = useSelector(state => state.attractions.attractions);
+  const dispatch = useDispatch();
+  const width = useWindowWidth(); // This needs to be a hook that returns the current window width
+  const [showPopup, setShowPopup] = useState(false);
 
-    const updateFavorites = (favorites, attractions) => {
-        return favorites.map(favorite => {
-            const updatedAttraction = attractions.find(attr => attr.id === favorite.id);
-            return updatedAttraction ? { ...favorite, waitTime: updatedAttraction.waitTime, status: updatedAttraction.status } : favorite;
-        });
-    };
+  const updateFavorites = useCallback((favorites, attractions) => {
+    return favorites.map(favorite => {
+      const updatedAttraction = attractions.find(attr => attr.id === favorite.id);
+      return updatedAttraction ? { ...favorite, waitTime: updatedAttraction.waitTime, status: updatedAttraction.status } : favorite;
+    });
+  }, []);
 
-    const removeFavorite = (favorite) => {
-        const updatedFavorites = reduxFavorites.filter(fav => fav.id !== favorite.id);
-        dispatch(setFavorites(updatedFavorites));
-    };
+  const removeFavorite = useCallback((favorite) => {
+    const updatedFavorites = reduxFavorites.filter(fav => fav.id !== favorite.id);
+    dispatch(setFavorites(updatedFavorites));
+  }, [dispatch, reduxFavorites]);
 
-    const getWaitTimeColor = (attraction) => {
-        if (attraction.status === 'CLOSED' || attraction.status === 'DOWN') {
-            return styles.gray;
-        } else if (attraction.waitTime <= 15) {
-            return styles.green;
-        } else if (attraction.waitTime <= 30) {
-            return styles.yellow;
-        } else {
-            return styles.red;
-        }
-    };
 
-    useEffect(() => {
-        const updatedFavorites = updateFavorites(reduxFavorites, attractions);
-        if (JSON.stringify(updatedFavorites) !== JSON.stringify(reduxFavorites)) {
-            dispatch(setFavorites(updatedFavorites));
-        }
-    }, [attractions, dispatch, reduxFavorites]);
+  useEffect(() => {
+    const updatedFavorites = updateFavorites(reduxFavorites, attractions);
+    if (JSON.stringify(updatedFavorites) !== JSON.stringify(reduxFavorites)) {
+      dispatch(setFavorites(updatedFavorites));
+    }
+  }, [attractions, dispatch, reduxFavorites, updateFavorites]);
 
-    useEffect(() => {
-        const shuffledAttractionNames = attractionNames.sort(() => 0.5 - Math.random());
-        const selectedAttractions = shuffledAttractionNames.slice(0, 6);
-        setRecommendedAttractions(selectedAttractions);
-    }, []);
+  useEffect(() => {
+    const shuffledAttractionNames = [...attractionNames].sort(() => 0.5 - Math.random());
+    const selectedAttractions = shuffledAttractionNames.slice(0, 6);
+    // Assuming setRecommendedAttractions is used to set state somewhere in this component or its children
+  }, [attractionNames]);
 
-    const closePopup = () => {
-        setShowPopup(false);
-    };
+  useEffect(() => {
+    // Vérifiez si userPreferences existe dans le localStorage
+    const userPreferences = localStorage.getItem('userPreferences');
+    if (!userPreferences) {
+      // S'il n'existe pas, affichez le popup
+      setShowPopup(true);
+    }
+  }, []);
 
-    const handleToggleFavorite = (attractionName) => {
-        const attraction = attractions.find(attr => attr.name === attractionName);
-        if (attraction) {
-            dispatch(toggleFavorite(attraction));
-        }
-    };
+  const closePopup = () => {
+    setShowPopup(false);
+  };
 
-    return (
-        <div className={styles.homePage}>
-            {width > 768 && <Navbar />}
-            <div className={styles.topcontainer}>
-                <div className={styles.heroSection}>
-                    <img src={backgroundImage} alt="Disneyland Paris" className={styles.heroImage} />
-                </div>
-            </div>
-            <div className={styles.bottomcontainer}>
-                <div className={styles.content}>
-                    {reduxFavorites.length > 0 ? (
-                        <div className={styles.attractionsSection}>
-                            {reduxFavorites.map(favorite => (
-                                <FavoriteCard
-                                    key={favorite.id}
-                                    favorite={favorite}
-                                    onRemove={removeFavorite}
-                                    getWaitTimeColor={getWaitTimeColor}
-                                />
-                            ))}
-                        </div>
-                    ) : (
-                        <div className={styles.noFavoritesMessage}>
-                            <p>Vous n'avez pas encore de favoris.</p>
-                            <Link to="/attractions" className={styles.linkButton}>
-                                Ajoutez votre première attraction
-                            </Link>
-                        </div>
-                    )}
-                </div>
-            </div>
-            {width <= 768 && <BottomNav />}
-            {showPopup && <PopupSurvey onClose={closePopup} attractions={attractions} />}
+  return (
+    <div className={styles.homePage}>
+      {width > 768 && <Navbar />}
+      <div className={styles.topcontainer}>
+        <div className={styles.heroSection}>
+          <img src={backgroundImage} alt="Disneyland Paris" className={styles.heroImage} />
         </div>
-    );
+      </div>
+      <div className={styles.bottomcontainer}>
+        <div className={styles.content}>
+          {reduxFavorites.length > 0 ? (
+            <div className={styles.attractionsSection}>
+              {reduxFavorites.map(favorite => (
+                <FavoriteCard
+                  key={favorite.id}
+                  favorite={favorite}
+                  onRemove={removeFavorite}
+                  getWaitTimeColor={getWaitTimeColor}
+                />
+              ))}
+            </div>
+          ) : (
+            <div className={styles.noFavoritesMessage}>
+              <p>Vous n'avez pas encore de favoris.</p>
+              <Link to="/attractions" className={styles.linkButton}>
+                Ajoutez votre première attraction
+              </Link>
+            </div>
+          )}
+        </div>
+      </div>
+      {width <= 768 && <BottomNav />}
+      {showPopup && <PopupSurvey onClose={() => setShowPopup(false)} attractions={attractions} />}
+    </div>
+  );
 };
 
 export default HomePage;
