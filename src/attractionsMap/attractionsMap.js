7a2dfb0ad7408c1e0b141d49bce8f2844f22ce21
HARDEMAN CHRISTOPHER <41175936+Chardema@users.noreply.github.com>
2024-03-17 19:51:59 +0100

    Mise à jour localisation temps réeel

--- src/attractionsMap/attractionsMap.js
+++ src/attractionsMap/attractionsMap.js
@@ -2,6 +2,9 @@ import React, { useState, useEffect } from 'react';
 import { MapContainer, TileLayer, Marker, Popup, Polyline } from 'react-leaflet';
 import axios from 'axios';
 import L from 'leaflet';
+import styles from  './attractionsMap.module.scss';
+// Importez simplify si vous utilisez une fonction de simplification des points
+// import { simplify } from 'simplify-js';
 
 const AttractionsMap = ({ attractions, getWaitTimeColor }) => {
     const [selectedAttraction, setSelectedAttraction] = useState(null);
@@ -11,61 +14,67 @@ const AttractionsMap = ({ attractions, getWaitTimeColor }) => {
     const [routeDuration, setRouteDuration] = useState(null);
 
     useEffect(() => {
-        const watchPosition = () => {
-            if ("geolocation" in navigator) {
-                // Commence à écouter les changements de position
-                const watchId = navigator.geolocation.watchPosition(
-                    (position) => {
-                        const { latitude, longitude } = position.coords;
-                        setUserLocation([latitude, longitude]);
-                        // Si une attraction est sélectionnée, recalculez l'itinéraire vers elle
-                        if (selectedAttraction) {
-                            calculateRoute([latitude, longitude], selectedAttraction.coordinates);
-                        }
-                    },
-                    (error) => {
-                        console.error("Erreur lors de l'accès à la localisation :", error);
-                    },
-                    {
-                        enableHighAccuracy: true,
-                        maximumAge: 0,
-                        timeout: 5000,
+        let watchId;
+        if ("geolocation" in navigator) {
+            // Commence à écouter les changements de position
+            watchId = navigator.geolocation.watchPosition(
+                (position) => {
+                    const { latitude, longitude } = position.coords;
+                    setUserLocation([latitude, longitude]);
+                    // Si une attraction est sélectionnée, recalculez l'itinéraire vers elle
+                    if (selectedAttraction) {
+                        calculateRoute([latitude, longitude], selectedAttraction.coordinates);
                     }
-                );
-
-                return () => navigator.geolocation.clearWatch(watchId);
-            } else {
-                console.log("La géolocalisation n'est pas prise en charge par ce navigateur.");
-            }
-        };
-
-        const watchId = watchPosition();
+                },
+                (error) => {
+                    console.error("Erreur lors de l'accès à la localisation :", error);
+                },
+                {
+                    enableHighAccuracy: true, // Utilisez la localisation haute précision si disponible
+                    maximumAge: 0, // Accepte seulement les données de localisation fraîches
+                    timeout: 5000, // Temps d'attente maximum pour obtenir la localisation
+                }
+            );
+        } else {
+            console.log("La géolocalisation n'est pas prise en charge par ce navigateur.");
+        }
 
         // Nettoyage lors du démontage du composant
         return () => {
-            if (watchId) {
+            if (watchId !== undefined) {
                 navigator.geolocation.clearWatch(watchId);
             }
         };
-    }, [selectedAttraction, userLocation]); // Ajoutez `userLocation` pour recalculer l'itinéraire si la position de l'utilisateur change
+    }, [selectedAttraction]); // Ajoutez `selectedAttraction` comme dépendance si vous voulez recalculer l'itinéraire lorsque l'attraction sélectionnée change
+
 
     const handleMarkerClick = (attraction) => {
         setSelectedAttraction(attraction);
-        calculateRoute(userLocation, attraction.coordinates);
+        if (userLocation && attraction.coordinates) {
+            calculateRoute(userLocation, attraction.coordinates);
+        }
     };
+    const handleRequestRoute = (destinationCoordinates) => {
+        if (userLocation && destinationCoordinates) {
+            calculateRoute(userLocation, destinationCoordinates);
+        } else {
+            alert("Localisation de l'utilisateur non disponible. Veuillez réessayer.");
+        }
+    };
+
 
     const calculateRoute = async (startCoordinates, destinationCoordinates) => {
         try {
-            const apiKey = process.env.apiurl;
+            const apiKey = '5b3ce3597851110001cf62483f9fb5d6194f46139e925f786fde38a0';
             const response = await axios.get(`https://api.openrouteservice.org/v2/directions/foot-walking?api_key=${apiKey}&start=${startCoordinates[1]},${startCoordinates[0]}&end=${destinationCoordinates[1]},${destinationCoordinates[0]}`);
             if (response.data.features && response.data.features.length > 0) {
                 const coordinates = response.data.features[0].geometry.coordinates;
                 const latLngs = coordinates.map(coord => [coord[1], coord[0]]);
-                setRoute({ geometry: { coordinates: latLngs } });
+                setRoute({geometry: {coordinates: latLngs}});
 
-                // Mise à jour de la durée de l'itinéraire
+                // Mettez à jour la durée de l'itinéraire ici
                 const durationSeconds = response.data.features[0].properties.segments.reduce((total, segment) => total + segment.duration, 0);
-                const durationMinutes = Math.round(durationSeconds / 60); // Conversion en minutes
+                const durationMinutes = Math.round(durationSeconds / 60); // Convertissez en minutes
                 setRouteDuration(durationMinutes);
 
                 setErrorMessage('');
@@ -82,10 +91,11 @@ const AttractionsMap = ({ attractions, getWaitTimeColor }) => {
         }
     };
 
+
     return (
         <MapContainer center={[48.872, 2.775]} zoom={15} style={{ height: '80vh', width: '100vw' }}>
             <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-            {attractions.map(attraction =>
+            {attractions.map((attraction) => (
                 attraction.coordinates && Array.isArray(attraction.coordinates) && attraction.coordinates.length === 2 ? (
                     <Marker
                         key={attraction.id}
@@ -93,26 +103,29 @@ const AttractionsMap = ({ attractions, getWaitTimeColor }) => {
                         icon={L.divIcon({
                             className: 'custom-marker-icon',
                             html: `
-                              <div class="marker-bubble">
-                                <div>${attraction.waitTime !== null ? `${attraction.waitTime} min` : 'Indispo'}</div>
-                              </div>`
-                          })}
-                        eventHandlers={{
-                            click: () => handleMarkerClick(attraction),
-                        }}
+        <div style="padding: 15px 15px; display: flex; justify-content: center; background-color: rgba(255, 255, 255, 0.9); border-radius: 5px; text-align: center;">
+            ${attraction.waitTime !== null ? `${attraction.waitTime} min` : 'Indispo'}
+        </div>
+    `
+                        })}
+
                     >
                         <Popup>
-                            {attraction.name}
-                            <br />
-                            {selectedAttraction && selectedAttraction.id === attraction.id && routeDuration && (
-                                <div>
-                                    Durée à pied : {routeDuration} min
-                                </div>
-                            )}
+                            <div>
+                                {attraction.name}
+                                <br />
+                                {selectedAttraction && selectedAttraction.id === attraction.id && routeDuration && (
+                                    <div>
+                                        Durée à pied : {routeDuration} min
+                                    </div>
+                                )}
+                                <button className="go-button" onClick={() => handleRequestRoute(attraction.coordinates)}>Go</button>
+                            </div>
                         </Popup>
                     </Marker>
                 ) : null
-            )}
+            ))}
+
             {route && route.geometry && Array.isArray(route.geometry.coordinates) && (
                 <Polyline
                     key={Date.now()} // Utilisez une valeur unique ici pour forcer le re-rendu
@@ -122,13 +135,8 @@ const AttractionsMap = ({ attractions, getWaitTimeColor }) => {
                     opacity={0.7}
                 />
             )}
-            {errorMessage && (
-                <div style={{ color: 'red', padding: 10, backgroundColor: 'white', borderRadius: 5 }}>
-                    {errorMessage}
-                </div>
-            )}
         </MapContainer>
     );
-            }
+};
 
-    export default AttractionsMap;
+export default AttractionsMap;
