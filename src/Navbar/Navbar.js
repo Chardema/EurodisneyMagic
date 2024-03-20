dbfb19eca27763832b40702424835cdd42e28782
HARDEMAN CHRISTOPHER <41175936+Chardema@users.noreply.github.com>
2024-03-20 16:35:34 +0100

    On commence les notes de patch : v 0.2
    - Interface légèrement amélioré
    - Mise en place des spectacles en Favori (encore pas mal de chose à Fix)

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -1,28 +1,16 @@
 import React from 'react';
 import { Link, useLocation } from 'react-router-dom';
-import styles from './Navbar.module.scss'
-import './Navbar.module.scss';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars } from '@fortawesome/free-solid-svg-icons';
+import styles from './Navbar.module.scss';
 
 const Navbar = () => {
-    const [menuOpen, setMenuOpen] = React.useState(false);
-    const location = useLocation();
-    const toggleMenu = () => {
-        setMenuOpen(!menuOpen);
-    }
-
     return (
         <nav className={styles.navbar}>
-            <div className={menuOpen ? `${styles.menuIcon} ${styles.open}` : styles.menuIcon} onClick={toggleMenu}>
-                <FontAwesomeIcon icon={faBars} />
-            </div>
-            <ul className={`${styles.navLinks} ${menuOpen ? styles.showMenu : ''}`}>
-                <li><Link to="/" onClick={toggleMenu}>Accueil</Link></li>
-                <li><Link to="/hours" onClick={toggleMenu}>Horaires</Link></li>
-                <li><Link to="/attractions" onClick={toggleMenu}>Attractions</Link></li>
-                <li><Link to="/spectacle" onClick={toggleMenu}>Spectacle</Link></li>
-                <li><Link to="/magicAITrip" onClick={toggleMenu}>MagicAITrip</Link></li>
+            <ul className={styles.navLinks}>
+                <li><Link to="/">Accueil</Link></li>
+                <li><Link to="/hours">Horaires</Link></li>
+                <li><Link to="/attractions">Attractions</Link></li>
+                <li><Link to="/spectacle">Spectacle</Link></li>
+                <li><Link to="/magicAITrip">MagicAITrip</Link></li>
             </ul>
         </nav>
     );
