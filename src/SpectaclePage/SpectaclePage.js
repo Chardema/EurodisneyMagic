c643aa18331e21fb4653d5302d2761cd561e4b12
HARDEMAN CHRISTOPHER <41175936+Chardema@users.noreply.github.com>
2024-03-20 16:35:34 +0100

    On commence les notes de patch : v 0.2
    - Interface légèrement amélioré
    - Mise en place des spectacles en Favori (encore pas mal de chose à Fix)

--- src/SpectaclePage/SpectaclePage.js
+++ src/SpectaclePage/SpectaclePage.js
@@ -1,81 +1,54 @@
 import React, { useState, useEffect } from 'react';
 import axios from "axios";
-import styles from './spectacle.module.scss';
+import { useDispatch, useSelector } from 'react-redux';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import L from 'leaflet';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHeart as solidHeart } from '@fortawesome/free-solid-svg-icons';
+import { faHeart as regularHeart } from '@fortawesome/free-regular-svg-icons';
+import { toggleFavoriteShow } from '../redux/actions'; // Assurez-vous que cette action existe et est correctement importée
 import Navbar from "../Navbar/Navbar";
-import {formatImageName, importImage, useWindowWidth} from "../utils";
-import BottomNav from "../mobileNavbar/mobileNavbar"; // Adaptez le chemin et le style en conséquence
+import BottomNav from "../mobileNavbar/mobileNavbar";
+import { formatImageName, importImage, useWindowWidth } from "../utils";
+import styles from './spectacle.module.scss';
 
-
-const showsNames = [
-    "Mickey's Dazzling Christmas Parade!",
-    'Let’s Sing Christmas!',
-    'Disney Dreams Nighttime Extravaganza',
-    'The Lion King: Rhythms of the Pride Lands',
-    'The Disney Junior Dream Factory',
-    'Frozen: A Musical Invitation',
-    'Stitch Live!',
-    'Mickey and the Magician',
-    'TOGETHER: a Pixar Musical Adventure',
-    'Avengers: Power the Night',
-    'Disney Stars on Parade ',
-    'Magic Over Disney: a nighttime show to the rhythm of Disney and Pixar Music',
-    'Guardians of the Galaxy: Dance Challenge! ',
-    'Disney Electrical Sky Parade',
-    'A Million Splashes of Colour'
-];
-const attractionShows = showsNames.reduce((acc, name) => {
-    const imageName = formatImageName(name);
-    acc[name] = importImage(imageName);
-    return acc;
-}, {});
 const Shows = () => {
-    const [lastUpdate, setLastUpdate] = useState(null);
     const [showsData, setShowsData] = useState([]);
-    const [viewMode, setViewMode] = useState('list'); // Ajoutez l'état pour gérer le mode d'affichage
-    const width = useWindowWidth()
+    const [viewMode, setViewMode] = useState('list');
+    const width = useWindowWidth();
+    const favorites = useSelector(state => state.favorites.favorites);
+    const dispatch = useDispatch();
 
-    const fetchData = async () => {
-        try {
-            const response = await axios.get('https://eurojourney.azurewebsites.net/api/shows');
-            const currentDateTime = new Date();
+    const handleToggleFavorite = (show) => {
+        // Déterminer si le spectacle est déjà dans les favoris
+        const isFavorited = favorites.some(fav => fav.id === show.id);
+        // Enrichir le spectacle avec un type avant de dispatcher
+        const showWithEntityType = { ...show, type: 'SHOW' };
+        dispatch(toggleFavoriteShow(isFavorited ? show.id : showWithEntityType));
+    };
 
-            const showsDataWithNextShowtime = response.data
-                .map(show => {
-                    // Trouver le prochain horaire de spectacle
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axios.get('https://eurojourney.azurewebsites.net/api/shows');
+                const currentDateTime = new Date();
+                const showsDataWithNextShowtime = response.data.map(show => {
                     const nextShowtime = show.showtimes
                         .filter(time => new Date(time.startTime) > currentDateTime)
                         .sort((a, b) => new Date(a.startTime) - new Date(b.startTime))[0];
-
-                    return {
-                        ...show,
-                        showtimes: nextShowtime ? [nextShowtime] : []
-                    };
-                })
-                .filter(show => show.showtimes.length > 0); // Exclure les spectacles sans horaires futurs
-
-            setShowsData(showsDataWithNextShowtime);
-            setLastUpdate(currentDateTime);
-        } catch (error) {
-            console.error('Erreur lors de la récupération des données:', error);
-        }
-    };
-
-
-    useEffect(() => {
+                    return { ...show, showtimes: nextShowtime ? [nextShowtime] : [] };
+                }).filter(show => show.showtimes.length > 0);
+                setShowsData(showsDataWithNextShowtime);
+            } catch (error) {
+                console.error('Erreur lors de la récupération des données:', error);
+            }
+        };
         fetchData();
         const intervalId = setInterval(fetchData, 60000);
         return () => clearInterval(intervalId);
     }, []);
 
-    // Fonction pour comparer les horaires des spectacles
-    const compareShowtimes = (a, b) => {
-        const timeA = new Date(a.showtimes[0].startTime).getTime();
-        const timeB = new Date(b.showtimes[0].startTime).getTime();
-
-        return timeA - timeB;
-    };
+    const compareShowtimes = (a, b) => new Date(a.showtimes[0]?.startTime).getTime() - new Date(b.showtimes[0]?.startTime).getTime();
 
     return (
         <div>
@@ -85,74 +58,39 @@ const Shows = () => {
                     <button onClick={() => setViewMode('list')}>Liste</button>
                     <button onClick={() => setViewMode('map')}>Plan</button>
                 </div>
-                <h1>Spectacles prévu aujourd'hui</h1>
-                <p className={styles.info}>Pour plus de précision, n'hésitez pas à consultez l'application Disneyland Paris officielle </p>
+                <h1>Spectacles prévus aujourd'hui</h1>
+                <p className={styles.info}>Pour plus de précision, n'hésitez pas à consulter l'application Disneyland Paris officielle.</p>
                 {viewMode === 'list' ? (
                     <div className={styles.showsList}>
-                        {showsData.length > 0 ? (
-                            showsData
-                                .sort(compareShowtimes)
-                                .map((show) => (
-                                <div key={show.id} className={styles.card}>
-                                    <img className={styles.showImage} src={attractionShows[show.name]} alt={show.name} />
-                                    <h3 className={styles.showName}>{show.name}</h3>
-                                    {show.showtimes.length > 0 ? (
-                                        <h2 className={styles.showStatus}>Prochaine représentation : {new Date(show.showtimes[0].startTime).toLocaleTimeString()}</h2>
-                                    ) : (
-                                        <p className={styles.noShowtimes}>Aucune représentation prévue.</p>
-                                    )}
-                                </div>
-                            ))
-                        ) : (
-                            <p>Aucun spectacle disponible pour le moment.</p>
-                        )}
+                        {showsData.length > 0 ? showsData.sort(compareShowtimes).map((show) => (
+                            <div key={show.id} className={styles.card}>
+                                <img className={styles.showImage} src={importImage(formatImageName(show.name))} alt={show.name} />
+                                <h3 className={styles.showName}>{show.name}</h3>
+                                {show.showtimes.length > 0 ? (
+                                    <h2 className={styles.showStatus}>Prochaine représentation : {new Date(show.showtimes[0].startTime).toLocaleTimeString()}</h2>
+                                ) : (
+                                    <p className={styles.noShowtimes}>Aucune représentation prévue.</p>
+                                )}
+                                <button className={styles.favoriteButton} onClick={() => handleToggleFavorite(show)}>
+                                    <FontAwesomeIcon icon={favorites.some(fav => fav.id === show.id && fav.type === 'SHOW') ? solidHeart : regularHeart} />
+                                </button>
+                            </div>
+                        )) : <p>Aucun spectacle disponible pour le moment.</p>}
                     </div>
                 ) : (
-                        <MapContainer
-                            center={[48.872, 2.775]} // Coordonnées centrales du parc
-                            zoom={15}
-                            scrollWheelZoom={true}
-                            style={{ height: '80vh', width: '100vw' } }
-                            maxBounds={[[48.850, 2.770], [48.877, 2.785]]} // Limite de déplacement
-                            minZoom={15} // Limite de zoom minimumm
-                        >
-                            <TileLayer
-                                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                            />
-                            {showsData.map((show) => {
-                                console.log(show);
-                                if (show.coordinates && show.coordinates.length === 2) {
-                                    return (
-                                        <Marker
-                                            key={show.id}
-                                            position={[show.coordinates[0], show.coordinates[1]]}
-                                            icon={L.icon({
-                                                iconUrl: attractionShows[show.name],
-                                                iconSize: [40, 40],
-                                                iconAnchor: [20, 40],
-                                                popupAnchor: [0, -40],
-                                            })}
-                                        >
-                                            <Popup>
-                                                <h3>{show.name}</h3>
-                                                {show.showtimes.length > 0 ? (
-                                                    <p>Prochaine représentation : {new Date(show.showtimes[0].startTime).toLocaleTimeString()}</p>
-                                                ) : (
-                                                    <p>Aucune représentation prévue.</p>
-                                                )}
-                                            </Popup>
-                                        </Marker>
-                                    );
-                                }
-                                return null; // Ne pas afficher de marqueur si les coordonnées ne sont pas disponibles
-                            })}
-                        </MapContainer>
+                    <MapContainer center={[48.872, 2.775]} zoom={15} scrollWheelZoom={true} style={{ height: '80vh', width: '100vw' }}>
+                        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+                        {showsData.map((show) => show.coordinates && show.coordinates.length === 2 && (
+                            <Marker key={show.id} position={show.coordinates} icon={L.icon({ iconUrl: importImage(formatImageName(show.name)), iconSize: [40, 40], iconAnchor: [20, 40], popupAnchor: [0, -40] })}>
+                                <Popup>{show.name} - Prochaine représentation : {new Date(show.showtimes[0]?.startTime).toLocaleTimeString()}</Popup>
+                            </Marker>
+                        ))}
+                    </MapContainer>
                 )}
             </div>
             <BottomNav />
         </div>
-    )
+    );
 };
 
 export default Shows;
--- src/popupSurvey/popupSurvey.js
+++ src/popupSurvey/popupSurvey.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback, useMemo } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import styles from './popupSurvey.module.scss';
 import { FaClock, FaHome, FaLaughBeam, FaHeart, FaTimesCircle } from 'react-icons/fa';
 import { TbRollercoaster } from 'react-icons/tb';
@@ -6,6 +6,7 @@ import { useDispatch } from 'react-redux';
 import { toggleFavorite } from '../redux/actions';
 import CardSwipe from '../CardSwipe/CardSwipe';
 import { attractionImages } from '../Attractions/AttractionsPage';
+import { useMemo } from 'react';
 
 const questionIcons = {
   0: <FaHome className={styles.icon} />,
@@ -43,6 +44,7 @@ const PopupSurvey = ({ onClose, attractions }) => {
     typePreference: [],
     waitTimePreference: '',
   });
+  const [showConfirmButton, setShowConfirmButton] = useState(false);
   const [currentAttractionIndex, setCurrentAttractionIndex] = useState(0);
   const dispatch = useDispatch();
   const [hasSwipedAllAttractions, setHasSwipedAllAttractions] = useState(false);
@@ -59,7 +61,7 @@ const PopupSurvey = ({ onClose, attractions }) => {
 
   const recommendedAttractions = useMemo(() => {
     if (!attractions || !responses.typePreference.length) return [];
-  
+
     let maxWaitTime;
     switch (responses.waitTimePreference) {
       case 'Moins de 15 minutes': maxWaitTime = 15; break;
@@ -68,12 +70,49 @@ const PopupSurvey = ({ onClose, attractions }) => {
       case 'Plus de 60 minutes': maxWaitTime = Infinity; break;
       default: maxWaitTime = Infinity;
     }
-  
+
     return attractions.filter(attraction =>
         (responses.typePreference.length === 0 || responses.typePreference.some(type => attraction.type.includes(type))) &&
         attraction.waitTime <= maxWaitTime
-      );
+    );
   }, [attractions, responses]);
+  const handleAnswer = useCallback((answer, type) => {
+    setResponses(current => {
+      const updatedResponses = {
+        ...current,
+        [type]: type === 'typePreference'
+            ? current.typePreference.includes(answer)
+                ? current.typePreference.filter(item => item !== answer)
+                : [...current.typePreference, answer]
+            : answer,
+      };
+
+      // Ajouté pour gérer la progression pour tous les types de questions
+      if (type !== 'typePreference' || answer === 'Suivant') {
+        setCurrentStep(current => current + 1);
+        setShowConfirmButton(false);
+      } else if (type === 'typePreference') {
+        setShowConfirmButton(updatedResponses.typePreference.length > 0);
+      }
+
+      return updatedResponses;
+    });
+  }, []);
+
+
+  useEffect(() => {
+    if (questions[currentStep]?.type === 'typePreference') {
+      setShowConfirmButton(responses.typePreference.length > 0);
+    } else {
+      setShowConfirmButton(false);
+    }
+  }, [currentStep, responses.typePreference]);
+
+  useEffect(() => {
+    if (currentStep === questions.length) {
+      localStorage.setItem('userPreferences', JSON.stringify(responses));
+    }
+  }, [currentStep, responses]);
 
   const onSwipeLeft = useCallback(() => {
     if (currentAttractionIndex < recommendedAttractions.length - 1) {
@@ -92,69 +131,63 @@ const PopupSurvey = ({ onClose, attractions }) => {
     }
   }, [dispatch, currentAttractionIndex, recommendedAttractions.length]);
 
-  const handleAnswer = useCallback((answer, type) => {
-    setResponses(current => ({
-      ...current,
-      [type]: type === 'typePreference'
-        ? current.typePreference.includes(answer)
-          ? current.typePreference.filter(item => item !== answer)
-          : [...current.typePreference, answer]
-        : answer,
-    }));
-    setCurrentStep(current => current + 1);
-  }, []);
-
-  useEffect(() => {
-    if (currentStep === questions.length) {
-      localStorage.setItem('userPreferences', JSON.stringify(responses));
-    }
-  }, [currentStep, responses]);
-
   return (
-    <div className={styles.popupContainer}>
-      <div className={styles.popup}>
-        {currentStep < questions.length ? (
-          <>
-            {questionIcons[currentStep]}
-            <h2>{questions[currentStep].question}</h2>
-            {questions[currentStep].answers.map((answer, index) => (
-              <button
-                key={index}
-                onClick={() => handleAnswer(answer, questions[currentStep].type)}
-                className={`${styles.answerButton} ${(responses[questions[currentStep].type] || []).includes(answer) ? styles.selected : ''}`}
-              >
-                {answer}
-              </button>
-            ))}
-          </>
-        ) : (
-          <>
-            <h2>Attractions recommandées pour vous</h2>
-            {recommendedAttractions.length > 0 && !hasSwipedAllAttractions ? (
+      <div className={styles.popupContainer}>
+        <div className={styles.popup}>
+          {currentStep < questions.length ? (
               <>
-                <CardSwipe
-                  key={recommendedAttractions[currentAttractionIndex].id}
-                  attraction={{ 
-                    ...recommendedAttractions[currentAttractionIndex],
-                    image: attractionImages[recommendedAttractions[currentAttractionIndex].name]
-                  }}
-                  onSwipeLeft={onSwipeLeft}
-                  onSwipeRight={() => onSwipeRight(recommendedAttractions[currentAttractionIndex])}
-                />
-                <div className={styles.actionButtons}>
-                  <FaTimesCircle className={styles.passIcon} onClick={onSwipeLeft} />
-                  <FaHeart className={styles.favoriteIcon} onClick={() => onSwipeRight(recommendedAttractions[currentAttractionIndex])} />
-                </div>
+                {questionIcons[currentStep]}
+                <h2>{questions[currentStep].question}</h2>
+                {questions[currentStep].answers.map((answer, index) => (
+                    <button
+                        key={index}
+                        onClick={() => handleAnswer(answer, questions[currentStep].type)}
+                        className={`${styles.answerButton} ${(responses[questions[currentStep].type] || []).includes(answer) ? styles.selected : ''}`}
+                    >
+                      {answer}
+                    </button>
+                ))}
+                {showConfirmButton && (
+                    <button
+                        onClick={() => {
+                          setCurrentStep(current => current + 1);
+                          setShowConfirmButton(false); // Cacher le bouton après le passage à la prochaine question
+                        }}
+                        className={styles.confirmButton}
+                    >
+                      Confirmer
+                    </button>
+                )}
               </>
-            ) : (
-              <p>Toutes les attractions recommandées ont été parcourues. Appuyez sur "Fermer" pour continuer.</p>
-            )}
-            <button onClick={onClose} className={styles.closeButton}>Fermer</button>
-          </>
-        )}
+          ) : (
+              <>
+                <h2>Attractions recommandées pour vous</h2>
+                {recommendedAttractions.length > 0 && !hasSwipedAllAttractions ? (
+                    <>
+                      <CardSwipe
+                          key={recommendedAttractions[currentAttractionIndex].id}
+                          attraction={{
+                            ...recommendedAttractions[currentAttractionIndex],
+                            image: attractionImages[recommendedAttractions[currentAttractionIndex].name]
+                          }}
+                          onSwipeLeft={onSwipeLeft}
+                          onSwipeRight={() => onSwipeRight(recommendedAttractions[currentAttractionIndex])}
+                      />
+                      <div className={styles.actionButtons}>
+                        <FaTimesCircle className={styles.passIcon} onClick={onSwipeLeft} />
+                        <FaHeart className={styles.favoriteIcon} onClick={() => onSwipeRight(recommendedAttractions[currentAttractionIndex])} />
+                      </div>
+                    </>
+                ) : (
+                    <p>Toutes les attractions recommandées ont été parcourues. Appuyez sur "Fermer" pour continuer.</p>
+                )}
+                <button onClick={onClose} className={styles.closeButton}>Fermer</button>
+              </>
+          )}
+        </div>
       </div>
-    </div>
   );
 };
 
 export default PopupSurvey;
+
